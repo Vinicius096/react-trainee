127a2e4619e74da09582b22339f84b53de60de45
Vinicius Brenner <vinicius_brennerr@hotmail.com>
2020-10-08 11:11:17 -0300

    Criando componentes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import ListaDeNotas from "./components/ListaDeNotas"
 
 function App() {
   return (
-    <h1>oie!!!</h1>
+    <div>
+      <form>
+      <input type="text" placeholder="Título"/>
+      <textarea placeholder="Escreva sua nota..."/>
+      <button>Criar nota</button>
+      </form>
+      <ListaDeNotas/>
+    </div>
+    
+    
   );
 }
 
