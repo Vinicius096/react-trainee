9726c8f47cec3e9fb5509956e9b94a10cea905bb
Vinicius Brenner <vinicius_brennerr@hotmail.com>
2020-10-13 09:58:24 -0300

    Estilizando componentes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, {Component} from 'react'
-import ListaDeNotas from "./components/ListaDeNotas"
-import FomularioCadastro from "./components/FormularioCadastro"
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
 
-function App() {
-  return (
-    <div>
-      <FomularioCadastro/>
-      <ListaDeNotas/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 export default App;
